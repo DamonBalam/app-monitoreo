d4619d1437c4c694c6ca0c172d9e260b44591bb7
Arturo Saldivar <damonbalam@gmail.com>
2020-03-02 15:41:26 -0500

    first commit

---
+++ src/components/SideComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    color="light-blue darken-4"
+    :expand-on-hover="expandOnHover"
+    :mini-variant="miniVariant"
+    :right="false"
+    :src="bg"
+    app
+    dark
+    clipped
+  >
+    <v-list dense nav class="py-0">
+      <v-list-item two-line :class="miniVariant && 'px-0'" @click="menu">
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Arturo Saldivar</v-list-item-title>
+          <v-list-item-subtitle>USU001</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn block>Cerrar Sesión</v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Tickets", icon: "mdi-cards" },
+        { title: "Configuración", icon: "mdi-cogs" }
+      ],
+      color: "primary",
+      colors: ["primary", "blue", "success", "red", "teal"],
+      right: false,
+      miniVariant: false,
+      expandOnHover: false,
+      background: false
+    };
+  },
+  methods: {
+    menu() {
+      this.$store.commit("SET_DRAWER");
+    }
+  },
+  computed: {
+    drawer() {
+      return this.$store.state.drawer;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
